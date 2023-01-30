3d1b0e0c7029769cb6545a6e74ba03ede9606070
Krzysztof Grzegorowski <k-grzegorowski@wp.pl>
2023-01-30 18:54:46 +0100

    15.3

--- tasks/src/main/java/com/crud/tasks/service/DbService.java
+++ tasks/src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,13 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.TaskRepository.TaskRepository;
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +17,14 @@ public class DbService {
 
     public List<Task> getAllTasks() {
         return repository.findAll();
+    }
+
+    public Task getTask(final Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(final Task task){
+        return repository.save(task);
     }
 
 
